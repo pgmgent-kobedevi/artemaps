81cf608bc45deea369189c118c819f7d44a82b55
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2021-04-09 17:05:01 +0200

    updated title

--- movie-db-app/src/components/App/Header/Header.js
+++ movie-db-app/src/components/App/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap shadow">
-            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">Time tracking app</a>
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">Movie DB</a>
             <ul className="navbar-nav px-3">
                 <li className="nav-item text-nowrap">
                     <LogoutButton/>
