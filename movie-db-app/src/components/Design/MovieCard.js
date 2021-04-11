ca7448cf99a95f2cfcde86c1e9a446fac69d997f
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2021-04-11 02:52:17 +0200

    WIP like

--- movie-db-app/src/components/Design/MovieCard.js
+++ movie-db-app/src/components/Design/MovieCard.js
@@ -2,14 +2,15 @@ import { Link } from "react-router-dom";
 import { route, Routes } from "../../core/routing";
 import Like from "../App/Movies/Like/Like";
 
-const MovieCard = ({movie, onUpdate, onError}) => {
+const MovieCard = ({movie, id, onUpdate, onError}) => {
 
     return (
         <div className='movieCard'>
             <Like 
                 onUpdate={onUpdate} 
                 onError={onError}
-                id={movie._id}
+                movieId={movie._id}
+                id={id}
             />
             <Link to={route(Routes.MoviesDetail, {id: movie._id})}>
                 <img src={movie.coverLink} alt='Movie poster'/>
