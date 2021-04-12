3dc2904d9ccd67ba825a760bf6a63ca28fd5d20a
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2021-04-13 01:24:49 +0200

    updated to latest useFetch

--- movie-db-app/src/core/hooks/useFetch.js
+++ movie-db-app/src/core/hooks/useFetch.js
@@ -1,35 +1,97 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useEffect, useCallback, useReducer } from 'react';
 import useAuthApi from './useAuthApi';
+
+const initialState = {
+    data: null,
+    error: null,
+    page: 1,
+};
+
+const Actions = {
+    Prev: 'prevPage',
+    Next: 'nextPage',
+    Data: 'setData',
+    Error: 'setError',
+    Clear: 'clear',
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case Actions.Prev:
+            return {
+                ...state,
+                page: Math.max(state.page - 1, 1),
+                data: null,
+            };
+        case Actions.Next:
+            return {
+                ...state,
+                page: state.page + 1,
+                data: null,
+            };
+        case Actions.Clear:
+            return {
+                ...state,
+                data: null,
+                error: null,
+            };
+        case Actions.Data:
+            return {
+                ...state,
+                data: action.payload,
+            };
+        case Actions.Error:
+            return {
+                ...state,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
 
 const useFetch = (apiCall) => {
     const withAuth = useAuthApi();
 
-    const [data, setData] = useState();
-    const [error, setError] = useState();
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    const fetchData = useCallback((isCurrent = true) => {
-        withAuth(apiCall())
-        .then((data) => isCurrent && setData(data))
-        .catch((error) => {
-            isCurrent && setError(error);
-        });
-    }, [apiCall, withAuth]);
+    const { data, error } = state;
+
+    const nextPage = useCallback(() => dispatch({ type: Actions.Next }), []);
+    const prevPage = useCallback(() => dispatch({ type: Actions.Prev }), []);
+    const setData = useCallback(
+        (data) => dispatch({ type: Actions.Data, payload: data }),
+        []
+    );
+    const setError = useCallback(
+        (error) => dispatch({ type: Actions.Error, payload: error }),
+        []
+    );
+
+    const fetchData = useCallback(
+        () => {
+            let isCurrent = true;
+
+            withAuth(apiCall())
+                .then((data) => isCurrent && setData(data))
+                .catch((error) => isCurrent && setError(error));
+
+            return () => {
+                isCurrent = false;
+            }
+        },
+        [setData, setError, apiCall, withAuth]
+    );
 
     const refresh = () => {
         fetchData();
     };
 
     useEffect(() => {
-        setData(null);
-        setError(null);
+        dispatch({ type: Actions.Clear });
         if (apiCall) {
-            let isCurrent = true;
-
-            fetchData(isCurrent);
-
-            return () => {
-                isCurrent = false
-            };
+            const cleanUp = fetchData();
+            return cleanUp;
         }
     }, [apiCall, fetchData]);
 
@@ -39,10 +101,11 @@ const useFetch = (apiCall) => {
         data,
         setData,
         error,
-        setError,
+        nextPage,
+        prevPage,
         refresh,
         isLoading,
-    }
+    };
 };
 
 export default useFetch;
