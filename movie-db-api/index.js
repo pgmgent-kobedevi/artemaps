56302582c0336e49723f18c06c6b9b481c298aba
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2021-04-15 21:07:51 +0200

    heroku update

--- movie-db-api/index.js
+++ movie-db-api/index.js
@@ -14,7 +14,7 @@ db.connect();
 
 const hostname = 'localhost';
 const app = express();
-const port = process.env.NODE_PORT;
+const port = process.env.PORT || 80;
 
 // register middleware
 registerMiddleware(app);
